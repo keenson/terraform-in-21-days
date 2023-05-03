1f1d9c9ae4c4ca30219da5bed4dc7ea1c57547e3
Olalekan Fadele <os.fadele@gmail.com>
2023-05-03 21:57:33 +0100

    Created vpc network components

--- main.tf
+++ main.tf
@@ -1,22 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-02396cdd13e9a1257"
-  instance_type = "t3.nano"
+# resource "aws_instance" "app_server" {
+#   ami           = "ami-02396cdd13e9a1257"
+#   instance_type = "t3.nano"
 
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
-}
+#   tags = {
+#     Name = "ExampleAppServerInstance"
+#   }
+# }
