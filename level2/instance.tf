ac85bd20e794af05211104ea54b8c5a963ed71e9
Olalekan Fadele <os.fadele@gmail.com>
2023-05-16 17:45:32 +0100

    Added load balancer

--- level2/instance.tf
+++ level2/instance.tf
@@ -21,7 +21,6 @@ resource "aws_instance" "public" {
   key_name                    = "devops"
   vpc_security_group_ids      = [aws_security_group.public.id]
   subnet_id                   = data.terraform_remote_state.level1.outputs.public_subnet_id[0]
-  user_data                   = file("user-data.sh")
 
 
   tags = {
@@ -70,9 +69,10 @@ resource "aws_instance" "private" {
   instance_type          = "t3.micro"
   key_name               = "devops"
   vpc_security_group_ids = [aws_security_group.private.id]
-  subnet_id              = data.terraform_remote_state.level1.outputs.private_subnet_id[0]
+  subnet_id              = data.terraform_remote_state.level1.outputs.private_subnet_id[1]
+  user_data              = file("user-data.sh")
   tags = {
-    Name = "${var.env_code}-public"
+    Name = "${var.env_code}-private"
   }
 }
 
@@ -88,6 +88,15 @@ resource "aws_security_group" "private" {
     protocol    = "tcp"
     cidr_blocks = [data.terraform_remote_state.level1.outputs.vpc_cidr]
   }
+
+  ingress {
+    description     = "HTTP from load balancer"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.load_balancer.id]
+  }
+
 
   egress {
     from_port   = 0
